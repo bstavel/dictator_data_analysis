520590972e8869ccb6c9fc0618151bc683bbaa59
bstavel <bstavel@stanford.edu>
2020-01-14 16:25:53 -0800

    hg data and eda

---
+++ R/load_high_gamma_data.R
@@ -0,0 +1,16 @@
+load_high_gamma_data <- function(file_path_to_hg_data, file_path_to_electrode_names) {
+  # load data #
+  hg_data <- read.csv(file_path_to_hg_data, stringsAsFactors = F, nrows = 18924, header = F, comment.char="")
+  # rename columns #
+  hg_data <- hg_data %>%
+    rename(index = V3000) %>%
+    rename(trial = V3001)
+  colnames(hg_data) <- gsub("^V", "bin_", colnames(hg_data))
+  
+  # merge with electrode data #
+  electrode_names <- read.csv(file_path_to_electrode_names)
+  hg_data <- merge.data.frame(hg_data, electrode_names, by = "index", all = T)
+  hg_data <- hg_data %>% select(-index) %>% rename(electrodes = Var1)
+  
+  return(hg_data)
+}
---
+++ R/stretch_start_end.R
@@ -0,0 +1,48 @@
+stretch_start_end <- function(stretch) {
+#####################################################################
+##  function to identify the longest stretchs of sig p vals.
+## input: 
+##    stretch: logical vec where T indicates sig p val.
+## output:
+##    sun_indicies: begining and ending inex of longest stretch.
+####################################################################
+  # intialize values #
+  streak_counter <- 0
+  max_streak <- 0
+  
+  # loop over stretch
+  for(idx in 1:length(stretch)) {
+    # if stretch == 1 start counting run #
+    if (stretch[idx] == T) {
+      # if no current streak, grab index #
+      if (streak_counter == 0){
+        beg_idx <- idx
+      }
+      
+      # add to count #
+      streak_counter <- streak_counter + 1
+      
+      # if streak counter is length of stetch, set indicies #
+      if (streak_counter == length(stretch)) {
+      run_indices <- c(1, length(stretch))
+      }
+    # if stretch idx is 0, end previous streaks, if max streak finishes store indices #
+    } else if (stretch[idx] == F) {
+      max_streak <- max(max_streak, streak_counter)
+      
+      if (max_streak == 0) {
+        run_indices <- NA
+      } else if (max_streak == streak_counter) {
+        run_indices <- c(beg_idx, idx)
+      }
+      # reset streak counter #
+      streak_counter <- 0
+    
+    # throw error if not 1 or 0 #
+    } else {
+      print("stretch should be a binary vec")
+    }
+    
+  }
+  return(run_indices)
+}
