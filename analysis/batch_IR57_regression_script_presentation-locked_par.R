42d78c466e6c448ea1cc5730dc32c5b1d91a9fe4
bstavel <bstavel@stanford.edu>
2020-05-13 09:48:30 -0700

    bug in IR57 presentation script again

--- analysis/batch_IR57_regression_script_presentation-locked_par.R
+++ analysis/batch_IR57_regression_script_presentation-locked_par.R
@@ -30,7 +30,7 @@ nCores <- 5
 registerDoParallel(nCores)
 
 ## read in data ##
-file_path_to_elecs_of_interest <- path(here(), "munge", "IR157_elecs_of_interest_insula.csv")
+file_path_to_elecs_of_interest <- path(here(), "munge", "IR57_elecs_of_interest_insula.csv")
 path_hg_clean <- path(here(), "munge", "IR57", "hg_behave_presentation_locked_extended_150.csv")
 hg_behave <-  read.csv(path_hg_clean)
 elecs_to_use <- read.csv(file_path_to_elecs_of_interest)
