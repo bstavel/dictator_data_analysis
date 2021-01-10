6c624a0316de5f1b502ed3580985a2aacdd05ae3
Brooke <bstavel@NEW-C02CV6C8ML85.local>
2021-01-10 10:12:18 -0800

    back to 32 for savio

--- analysis/batch_subject_scripts/batch_all_subs_multiple_regression_script_pres-locked_par.R
+++ analysis/batch_subject_scripts/batch_all_subs_multiple_regression_script_pres-locked_par.R
@@ -26,7 +26,7 @@ source(path(here(), "R", "run_filtered_anova.R"))
 source(path(here(), "R", 'mutate_cond.R'))
 
 ## paralellization ##
-nCores <- 10
+nCores <- 32
 registerDoParallel(nCores)
 
 ## regression parameters ##
